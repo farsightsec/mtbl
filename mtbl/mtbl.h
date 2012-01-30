937935ae4e76ca47d504e20c82f29d93aa82a658
Robert Edmonds <edmonds@isc.org>
2012-01-30 09:19:21 -0500

    mtbl: whitespace/comment tweaks

--- mtbl/mtbl.h
+++ mtbl/mtbl.h
@@ -25,21 +25,24 @@ extern "C" {
 #include <stdbool.h>
 #include <stdint.h>
 
+/* writer */
 struct mtbl_writer;
 struct mtbl_writer *	mtbl_writer_init(const char *fname);
+void			mtbl_writer_destroy(struct mtbl_writer **w);
+void			mtbl_writer_add(struct mtbl_writer *w,
+					const uint8_t *key, size_t len_key,
+					const uint8_t *val, size_t len_val);
 
-void		mtbl_writer_destroy(struct mtbl_writer **w);
-void		mtbl_writer_add(struct mtbl_writer *w,
-				const uint8_t *key, size_t len_key,
-				const uint8_t *val, size_t len_val);
-
+/* crc32c */
 uint32_t	mtbl_crc32c(const uint8_t *buffer, size_t length);
 
+/* fixed */
 size_t		mtbl_fixed_encode32(uint8_t *dst, uint32_t value);
 size_t		mtbl_fixed_encode64(uint8_t *dst, uint64_t value);
 uint32_t	mtbl_fixed_decode32(const uint8_t *ptr);
 uint64_t	mtbl_fixed_decode64(const uint8_t *ptr);
 
+/* varint */
 unsigned	mtbl_varint_length(uint64_t v);
 unsigned	mtbl_varint_length_packed(const uint8_t *buf);
 size_t		mtbl_varint_encode32(uint8_t *ptr, uint32_t value);
