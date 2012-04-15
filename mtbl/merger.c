bfd8dfff281d4327ec537513203bc0a9c699d86a
Robert Edmonds <edmonds@isc.org>
2012-04-15 16:25:08 -0400

    merger: merger_iter_add_entry(): handle case where iterator is NULL after initial entry_fill()

--- mtbl/merger.c
+++ mtbl/merger.c
@@ -276,8 +276,14 @@ merger_iter_add_entry(struct merger_iter *it, struct mtbl_iter *ent_it)
 	ent->val = ubuf_init(256);
 	ent->it = ent_it;
 	entry_fill(ent);
-	heap_push(it->h, ent);
-	entry_vec_add(it->entries, ent);
+	if (ent->it == NULL) {
+		ubuf_destroy(&ent->key);
+		ubuf_destroy(&ent->val);
+		free(ent);
+	} else {
+		heap_push(it->h, ent);
+		entry_vec_add(it->entries, ent);
+	}
 }
 
 static struct mtbl_iter *
