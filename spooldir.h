ce49efc240eea302f4c3ab59141b18a5eda38db0
Robert Edmonds <edmonds@isc.org>
2013-05-10 12:53:14 -0400

    my/spooldir: rsf -> my

--- spooldir.h
+++ spooldir.h
@@ -1,5 +1,5 @@
-#ifndef RSF_SPOOLDIR_H
-#define RSF_SPOOLDIR_H
+#ifndef MY_SPOOLDIR_H
+#define MY_SPOOLDIR_H
 
 struct spooldir;
 
@@ -7,4 +7,4 @@ struct spooldir *spooldir_init(const char *path);
 void spooldir_destroy(struct spooldir **);
 char *spooldir_next(struct spooldir *);
 
-#endif /* RSF_SPOOLDIR_H */
+#endif /* MY_SPOOLDIR_H */
