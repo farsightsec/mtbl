b99df7427100fd92fb66301a6131ffee36970cbf
Robert Edmonds <edmonds@fsi.io>
2013-11-26 21:00:39 -0500

    my/my_byteorder.h: use WORDS_BIGENDIAN

--- my_byteorder.h
+++ my_byteorder.h
@@ -3,15 +3,31 @@
 
 #include "config.h"
 
-#if !defined(IS_LITTLE_ENDIAN)
-# error "IS_LITTLE_ENDIAN must be defined to 0 or 1"
-#endif
-
 #ifdef HAVE_ENDIAN_H
 # include <endian.h>
 #else
 # ifdef HAVE_SYS_ENDIAN_H
 #  include <sys/endian.h>
+# endif
+#endif
+
+#if HAVE_DECL_HTOLE32
+# define my_htole32 htole32
+#else
+# if defined(WORDS_BIGENDIAN)
+#  define my_htole32 my_bswap32
+# else
+#  define my_htole32(x) (x)
+# endif
+#endif
+
+#if HAVE_DECL_LE32TOH
+# define my_le32toh le32toh
+#else
+# if defined(WORDS_BIGENDIAN)
+#  define my_le32toh my_bswap32
+# else
+#  define my_le32toh(x) (x)
 # endif
 #endif
 
@@ -23,25 +39,5 @@ my_bswap32(uint32_t x)
 		((x >>  8) & 0x0000ff00 ) |
 		((x >> 24) & 0x000000ff );
 }
-
-#if HAVE_DECL_HTOLE32
-# define my_htole32 htole32
-#else
-# if IS_LITTLE_ENDIAN
-#  define my_htole32(x) (x)
-# else
-#  define my_htole32 my_bswap32
-# endif
-#endif
-
-#if HAVE_DECL_LE32TOH
-# define my_le32toh le32toh
-#else
-# if IS_LITTLE_ENDIAN
-#  define my_le32toh(x) (x)
-# else
-#  define my_le32toh my_bswap32
-# endif
-#endif
 
 #endif /* MY_BYTEORDER_H */
