9dad7d138e561c0e476985bad9f7394a76100c78
Robert Edmonds <edmonds@fsi.io>
2014-03-28 17:39:46 -0400

    reader: get_block(): increase initial zlib decompression buffer size
    after the previous commit, we now correctly handle the case where the
    decompression buffer is (initially) too small to hold the decompressed
    output. however, we can still increase the initial size of this buffer
    in order to reduce the amount of looping and reallocation that has to be
    done in that case. so, increase it from 2X to 4X of the data block size.

--- mtbl/reader.c
+++ mtbl/reader.c
@@ -252,7 +252,7 @@ get_block(struct mtbl_reader *r, uint64_t offset)
 		break;
 	case MTBL_COMPRESSION_ZLIB:
 		needs_free = true;
-		block_contents_size = 2 * r->t.data_block_size;
+		block_contents_size = 4 * r->t.data_block_size;
 		get_block_zlib_decompress(raw_contents, raw_contents_size,
 					  &block_contents, &block_contents_size);
 		break;
