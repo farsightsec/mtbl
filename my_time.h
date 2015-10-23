8926c7c152f007154cd05f933d2b5580fa71228a
Henry Stern <stern@fsi.io>
2015-10-23 14:29:15 -0300

    my/my_time: Add my_timespec_cmp() function

--- my_time.h
+++ my_time.h
@@ -50,6 +50,27 @@ my_timespec_sub(const struct timespec *a, struct timespec *b)
 	}
 }
 
+/*
+ * Comparison function for struct timespec suitable for use with bsearch,
+ * qsort, etc.  Returns an integer less than, equal to, or greater than zero
+ * if the first argument is considered to be respectively less than, equal
+ * to, or greater than the second.
+ */
+static inline int
+my_timespec_cmp(const struct timespec *a, const struct timespec *b)
+{
+	if (a->tv_sec < b->tv_sec)
+		return (-1);
+	else if (a->tv_sec > b->tv_sec)
+		return (1);
+	else if (a->tv_nsec < b->tv_nsec)
+		return (-1);
+	else if (a->tv_nsec > b->tv_nsec)
+		return (1);
+	else
+		return (0);
+}
+
 static inline double
 my_timespec_to_double(const struct timespec *ts)
 {
