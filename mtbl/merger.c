c0f2c4032d52dd4ec88da3c1bd823b67de4ffdbb
Chris Mikkelson <cmikk@fsi.io>
2019-03-21 13:25:23 -0500

    Add 'dupsort' function merger option.
    When used with a NULL merge function, this will sort the results
    with duplicate keys based on their data.

--- mtbl/merger.c
+++ mtbl/merger.c
@@ -46,6 +46,8 @@ struct merger_iter {
 struct mtbl_merger_options {
 	mtbl_merge_func			merge;
 	void				*merge_clos;
+	mtbl_dupsort_func		dupsort;
+	void				*dupsort_clos;
 };
 
 struct mtbl_merger {
@@ -95,6 +97,14 @@ mtbl_merger_options_set_merge_func(struct mtbl_merger_options *opt,
 	opt->merge_clos = clos;
 }
 
+void
+mtbl_merger_options_set_dupsort_func(struct mtbl_merger_options *opt,
+				   mtbl_dupsort_func dupsort, void *clos)
+{
+	opt->dupsort = dupsort;
+	opt->dupsort_clos = clos;
+}
+
 struct mtbl_merger *
 mtbl_merger_init(const struct mtbl_merger_options *opt)
 {
@@ -140,6 +150,8 @@ _mtbl_merger_compare(const void *va, const void *vb, void *clos)
 {
 	const struct entry *a = (const struct entry *) va;
 	const struct entry *b = (const struct entry *) vb;
+	const struct mtbl_merger *m = (const struct mtbl_merger *)clos;
+	int res;
 
 	if (a->key == NULL && b->key == NULL)
 		return (0);
@@ -148,8 +160,16 @@ _mtbl_merger_compare(const void *va, const void *vb, void *clos)
 	if (b->key == NULL)
 		return (-1);
 
-	return (bytes_compare(ubuf_data(a->key), ubuf_size(a->key),
-			      ubuf_data(b->key), ubuf_size(b->key)));
+	res = bytes_compare(ubuf_data(a->key), ubuf_size(a->key),
+			    ubuf_data(b->key), ubuf_size(b->key));
+
+	if ((res == 0) && (m->opt.dupsort != NULL))
+		res = m->opt.dupsort(m->opt.dupsort_clos,
+				     ubuf_data(a->key), ubuf_size(a->key),
+				     ubuf_data(a->val), ubuf_size(a->val),
+				     ubuf_data(b->val), ubuf_size(b->val));
+
+	return res;
 }
 
 static mtbl_res
@@ -313,7 +333,7 @@ merger_iter_init(struct mtbl_merger *m)
 {
 	struct merger_iter *it = my_calloc(1, sizeof(*it));
 	it->m = m;
-	it->h = heap_init(_mtbl_merger_compare, NULL);
+	it->h = heap_init(_mtbl_merger_compare, m);
 	it->entries = entry_vec_init(source_vec_size(m->sources));
 	it->iters = iter_vec_init(source_vec_size(m->sources));
 	it->cur_key = ubuf_init(256);
