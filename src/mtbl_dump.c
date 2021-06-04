187a9ef950e08736073bb3b2e6d54b8be88a6d05
David Waitzman <djw@fsi.io>
2021-06-04 10:01:28 -0400

    add -x option to print data formatted as an 8 digit hex length, colon, the hex digit pairs separated by a dash

--- src/mtbl_dump.c
+++ src/mtbl_dump.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2014-2015 by Farsight Security, Inc.
+ * Copyright (c) 2012, 2014-2015, 2021 by Farsight Security, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,13 +18,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <assert.h>
 #include <mtbl.h>
 
 #include "libmy/print_string.h"
 
+static void print_hex_string(const void *data, size_t len, FILE *out);
+
+/* print data formatted as an 8 digit hex length, colon, the hex digit pairs separated by a dash */
+static void print_hex_string(const void *data, size_t len, FILE *out)
+{
+	uint8_t *str = (uint8_t *) data;
+        assert(len < 4294967295);
+        fprintf(out, "%08x:", (unsigned int)len);
+	while (len-- != 0) {
+		unsigned c = *(str++);
+                fprintf(out, "%02x", c);
+                if (len > 0)
+                        fputc('-', stdout);
+	}
+}
+
+
 static bool
-dump(const char *fname, const bool silent)
+dump(const char *fname, const bool silent, bool hex)
 {
 	const uint8_t *key, *val;
 	size_t len_key, len_val;
@@ -41,10 +58,17 @@ dump(const char *fname, const bool silent)
 	while (mtbl_iter_next(it, &key, &len_key, &val, &len_val)) {
 		if (silent)
 			continue;
-		print_string(key, len_key, stdout);
-		fputc(' ', stdout);
-		print_string(val, len_val, stdout);
-		fputc('\n', stdout);
+                if (hex) {
+                        print_hex_string(key, len_key, stdout);
+                        fputc(' ', stdout);
+                        print_hex_string(val, len_val, stdout);
+                        fputc('\n', stdout);
+                } else {
+                        print_string(key, len_key, stdout);
+                        fputc(' ', stdout);
+                        print_string(val, len_val, stdout);
+                        fputc('\n', stdout);
+                }
 	}
 	
 	mtbl_iter_destroy(&it);
@@ -65,12 +89,16 @@ main(int argc, char **argv)
 {
 	char *fname;
 	bool silent = false;
+        bool hex = false;
 
 	int c;
-	while ((c = getopt(argc, argv, "s")) != -1) {
+	while ((c = getopt(argc, argv, "sx")) != -1) {
 		switch (c) {
 		case 's':
 			silent = true;
+			break;
+		case 'x':
+			hex = true;
 			break;
 		default:
 			usage();
@@ -80,7 +108,7 @@ main(int argc, char **argv)
 		usage();
 	fname = argv[optind];
 
-	if (!dump(fname, silent))
+	if (!dump(fname, silent, hex))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
