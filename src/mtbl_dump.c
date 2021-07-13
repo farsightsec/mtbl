a593634845f102faa25b4bd4bdb6b7f0410b3df0
David Waitzman <djw@fsi.io>
2021-07-13 01:34:21 +0000

    Add '-K' AND '-V' options which take as arguments a positive integer which specifies the minimum length of the key or value, respectfully.

--- src/mtbl_dump.c
+++ src/mtbl_dump.c
@@ -44,10 +44,11 @@ static void print_hex_string(const void *data, size_t len, FILE *out)
 static bool
 dump(const char *fname, const bool silent, bool hex,
      const uint8_t *key_prefix, size_t key_prefix_len,
-     const uint8_t *val_prefix, size_t val_prefix_len)
+     const uint8_t *val_prefix, size_t val_prefix_len,
+     size_t key_min_len, size_t val_min_len)
 {
 	const uint8_t *key, *val;
-	size_t len_key, len_val;
+	size_t key_len, val_len;
 	struct mtbl_reader *r;
 	struct mtbl_iter *it;
 
@@ -58,30 +59,32 @@ dump(const char *fname, const bool silent, bool hex,
 	}
 
 	it = mtbl_source_iter(mtbl_reader_source(r));
-	while (mtbl_iter_next(it, &key, &len_key, &val, &len_val)) {
+	while (mtbl_iter_next(it, &key, &key_len, &val, &val_len)) {
 		if (key_prefix != 0
-		    && (len_key < key_prefix_len
+		    && (key_len < key_prefix_len
 			|| 0 != bcmp(key, key_prefix, key_prefix_len)))
 			continue;
 		if (val_prefix != 0
-		    && (len_val < val_prefix_len
+		    && (val_len < val_prefix_len
 			|| 0 != bcmp(val, val_prefix, val_prefix_len)))
+			continue;
+		if (key_len < key_min_len || val_len < val_min_len)
 			continue;
 		if (silent)
 			continue;
 		if (hex) {
-			print_hex_string(key, len_key, stdout);
+			print_hex_string(key, key_len, stdout);
 			fputc(' ', stdout);
-			print_hex_string(val, len_val, stdout);
+			print_hex_string(val, val_len, stdout);
 			fputc('\n', stdout);
 		} else {
-			print_string(key, len_key, stdout);
+			print_string(key, key_len, stdout);
 			fputc(' ', stdout);
-			print_string(val, len_val, stdout);
+			print_string(val, val_len, stdout);
 			fputc('\n', stdout);
 		}
 	}
-	
+
 	mtbl_iter_destroy(&it);
 	mtbl_reader_destroy(&r);
 
@@ -105,10 +108,12 @@ main(int argc, char **argv)
 	size_t key_prefix_len = 0;
 	uint8_t *val_prefix = NULL;
 	size_t val_prefix_len = 0;
+	size_t key_min_len = 0;
+	size_t val_min_len = 0;
 
 	int c;
 
-	while ((c = getopt(argc, argv, "sxk:v:")) != -1) {
+	while ((c = getopt(argc, argv, "sxk:v:K:V:")) != -1) {
 		switch (c) {
 		case 's':
 			silent = true;
@@ -136,6 +141,32 @@ main(int argc, char **argv)
 				return (EXIT_FAILURE);
 			}
 			break;
+		case 'K':
+			if (strlen(optarg) == 0) {
+				fprintf(stderr, "Need a non-empty argument to -K\n");
+				return (EXIT_FAILURE);
+			}
+
+			key_min_len = atoi(optarg);
+
+			if (key_min_len < 1) {
+				fprintf(stderr, "Bad value of minimum key length: %s\n", optarg);
+				return (EXIT_FAILURE);
+			}
+			break;
+		case 'V':
+			if (strlen(optarg) == 0) {
+				fprintf(stderr, "Need a non-empty argument to -K\n");
+				return (EXIT_FAILURE);
+			}
+
+			val_min_len = atoi(optarg);
+
+			if (val_min_len < 1) {
+				fprintf(stderr, "Bad value of minimum val length: %s\n", optarg);
+				return (EXIT_FAILURE);
+			}
+			break;
 		default:
 			usage();
 		}
@@ -145,7 +176,10 @@ main(int argc, char **argv)
 		usage();
 	fname = argv[optind];
 
-	if (!dump(fname, silent, hex, key_prefix, key_prefix_len, val_prefix, val_prefix_len))
+	if (!dump(fname, silent, hex,
+		  key_prefix, key_prefix_len,
+		  val_prefix, val_prefix_len,
+		  key_min_len, val_min_len))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
